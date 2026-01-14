861895d79f4914e82ca993eb41207e990d537704
vishal <18vishal.singh@gmail.com>
2026-01-14 00:27:18 -0800

    setting java version to 17

--- src/main/java/com/learning/project1/Project1Application.java
+++ src/main/java/com/learning/project1/Project1Application.java
@@ -1,13 +1,19 @@
 package com.learning.project1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project1Application {
+public class Project1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World!");
+    }
 
 }
